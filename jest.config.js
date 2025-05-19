c4e06d1e420413c4a1ffcd33ed5e90120c0c07d2
cobraSharman09 <142753267+cobraSharman09@users.noreply.github.com>
2025-05-19 12:25:30 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,27 @@
 module.exports = {
-    transform: {
-      "^.+\\.[tj]sx?$": "babel-jest"
-    },
-    testMatch: [
-      "**/?(*.)+(spec|test).[jt]s?(x)"
-    ],
-    testPathIgnorePatterns: [
-      "/node_modules/"
-    ],
-    collectCoverage: true,
-    coverageDirectory: "./coverage",
-    coverageReporters: ["lcov", "text", "html"],
-    testEnvironment: "jest-environment-jsdom",
-    collectCoverageFrom: [
-      "LAMD/**/*.js",
-      "!**/node_modules/**",
-      "!**/*.test.js"
-    ],
-    transformIgnorePatterns: [
-      "/node_modules/(?!(@babel|react)/)"
-    ]
-  };
-  
+  transform: {
+    "^.+\\.[tj]sx?$": "babel-jest"
+  },
+  testMatch: [
+    "**/?(*.)+(spec|test).[jt]s?(x)"
+  ],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/dist/"
+  ],
+  collectCoverage: true,
+  coverageDirectory: "./coverage",
+  coverageReporters: ["lcov", "text", "html"],
+  testEnvironment: "jsdom",
+  collectCoverageFrom: [
+    "**/*.js",
+    "!**/node_modules/**",
+    "!**/*.test.js",
+    "!**/*.spec.js",
+    "!coverage/**",
+    "!dist/**"
+  ],
+  transformIgnorePatterns: [
+    "/node_modules/"
+  ]
+};
