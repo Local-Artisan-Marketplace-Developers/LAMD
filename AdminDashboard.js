af92f23a204e148920349f9427bd209b97f76eb8
Kuhlle-16 <2603232@students.wits.ac.za>
2025-05-23 16:52:06 +0200

    Updated AdminDashboard.js

--- AdminDashboard.js
+++ AdminDashboard.js
@@ -1,23 +1,3 @@
-// Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js";
-  import { getAuth,GoogleAuthProvider ,signInWithCredential, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-auth.js";
-  import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-firestore.js";
-
-  const firebaseConfig = {
-    apiKey: "AIzaSyDV_AUjMqtpPMy5Yp4Wkwj-mCw9bCOE_-8",
-    authDomain: "artisanavenue-13a8d.firebaseapp.com",
-    projectId: "artisanavenue-13a8d",
-    storageBucket: "artisanavenue-13a8d.appspot.com",
-    messagingSenderId: "957096144049",
-    appId: "1:957096144049:web:d6c577dc7fec5a3e8b8690",
-    measurementId: "G-H2XGRYH35C"
-  };
-  const app = initializeApp(firebaseConfig);
-  const storage = getStorage(app);
-  const auth = getAuth(app);
-  const users = auth.currentUser;
-  const db = getFirestore(app);
-
 const logout = document.getElementById("logout");
 logout.addEventListener("click", async (e) => {
     e.preventDefault();
